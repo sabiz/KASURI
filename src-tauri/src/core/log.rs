8e61a2b577a06155254a8bff925bc98e3c6430d2
sAbIz <bellett@sabiz.jp>
2025-06-01 22:20:51 +0900

    [wip] migrate log impl

--- src-tauri/src/core/log.rs
+++ src-tauri/src/core/log.rs
@@ -1,3 +1,4 @@
+use rolling_file::{BasicRollingFileAppender, RollingConditionBasic};
 use std::sync::{LazyLock, Mutex};
 
 static INSTANCE: LazyLock<Mutex<Logger>> = LazyLock::new(|| {
@@ -11,6 +12,26 @@ pub fn init_logger() -> () {
     let top_dispatch = fern::Dispatch::new();
     let console_dispatch = fern::Dispatch::new().chain(std::io::stdout());
 
+    let log_dir = std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("logs");
+    if !log_dir.exists() {
+        std::fs::create_dir_all(&log_dir).expect("Failed to create log directory");
+    }
+    let log_file = BasicRollingFileAppender::new(
+        log_dir.join("KASURI.log"),
+        RollingConditionBasic::new().max_size(
+            10 * 1024 * 1024, // 10 MB
+        ),
+        5, // Keep 5 old log files
+    )
+    .expect("Failed to create rolling file appender");
+
+    let file_dispatch =
+        fern::Dispatch::new().chain(Box::new(log_file) as Box<dyn std::io::Write + Send>);
+
     top_dispatch
         .level(logger.level)
         .format(|out, message, record| {
@@ -22,6 +43,7 @@ pub fn init_logger() -> () {
             ))
         })
         .chain(console_dispatch)
+        .chain(file_dispatch)
         .apply()
         .expect("Failed to initialize logger");
 }
